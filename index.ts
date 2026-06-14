578667141b55ef935c17834c874eecff662c03e9
loiyHindi <Loiy.Hindi@cubesplatform.com>
2026-06-14 12:41:38 +0300

    a

--- index.ts
+++ index.ts
@@ -3,6 +3,4 @@ console.log("Starting app...");
 const sum = (a, b) => a + b;
 
 console.log("Result:", sum(2, 3));
-
-// Uncomment this to simulate failure:
-// throw new Error("Boom build should fail");
+console.log("App finished.");
